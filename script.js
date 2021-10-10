19f3956af8d2e2fc6c3caf22c82b115e0de1be6d
KJ <irateboolean@gmx.com>
2021-10-09 22:24:45 -0400

    basics of screen working

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const THE_DICTIONARY = {
+        'seven': 7,
+        'eight': 8,
+        'nine': 9,
+        'plus': 'plus',
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'minus': 'minus',
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'multiply': 'multiply',
+        'decimal': '.',
+        'zero': 0,
+        'delete': 'delete',
+        'divide': 'divide',
+        'equals': 'equals',
+        'clear': 'clear',
+}
+
 function add(a, b){
   return a + b
 }
@@ -27,4 +48,17 @@ function operate(a, b, func){
   }
 }
 
+function updateScreen(value){
+  console.log(value.target.textContent)
+  const screen = document.querySelector('.screen')
+  screen.innerText = `${screen.innerText}${value.target.textContent}`
+}
 
+// Add updateScreen to 0-9 & .
+const buttons = document.querySelectorAll('.btn')
+for (let i=0; i < buttons.length; i++){
+  if (!(isNaN(parseInt(buttons[i].textContent))) || (buttons[i].textContent == '.')) {
+    buttons[i].addEventListener('click', updateScreen)
+    console.log(buttons[i].textContent)
+  }
+}
